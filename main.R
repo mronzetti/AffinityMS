3e7130c44b745b33ab24bf52bf8e499a06b36fcc
ronzetti <51838549+mronzetti@users.noreply.github.com>
2022-11-14 08:00:08 -0500

    Working commit to split compounds into equal MW pools

--- main.R
+++ main.R
@@ -1,3 +1,31 @@
 library(tidyverse)
 
-df <- read.csv()
+df <- read.csv(file = './data/compoundlist.csv')
+numInGroup <- 15
+
+# Sort the entire list by MW in ascending order
+df <- arrange(df, MW)
+
+# Find the # of groups needed for the sorting
+numGroups <- as.integer(ceiling(nrow(df) / numInGroup))
+groupCount <- 1
+
+# Initialize groupID column
+df$groupID <- as.integer(0)
+
+# Attach the groupID num to each row
+for (x in 1:nrow(df)) {
+  df$groupID[x] <- groupCount
+  groupCount = groupCount + 1
+  if (groupCount == numGroups + 1)
+    groupCount = 1
+}
+
+splitDF <- split(df, df$groupID, drop = true)
+
+df$groupID <- as.factor(df$groupID)
+ggplot(df, aes(x = groupID, y = MW)) +
+  geom_boxplot() +
+  geom_point()
+
+write.csv(splitDF, './splitDF.csv')
